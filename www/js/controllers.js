eef2fc48025e3794e4cac3ee54c1617b1ebac864
YWJamesLin <YWJamesLin@gmail.com>
2017-01-08 22:19:40 +0800

    Init Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,60 @@
+angular.module ('app.controllers', ['app.services'])
+
+.controller ('MainCtrl', ['$scope', 'NCUOne', 'Share', 'intent', '$ionicPopup', '$rootScope', function ($scope, NCUOne, Share, intent, $ionicPopup, $rootScope) {
+  $scope.inputLink = "";
+  $scope.shortedLink = "";
+  $rootScope.$on('intent', function() {
+    $scope.inputLink = intent.getData ();
+    $scope.$apply ();
+  });
+  $scope.getShortLink = function (link) {
+    console.log (link);
+    var result = NCUOne.shortLink (link);
+    // the same source link
+    if (result == 1) {
+      $ionicPopup.show ({
+        'title' : '縮過了囉',
+        scope : $scope,
+        buttons : [
+          {
+            text : "好",
+            type : "button-positive"
+          }
+        ]
+      });
+    } else if (result == 0) {
+      $ionicPopup.show ({
+        'title' : '未知異常',
+        scope : $scope,
+        buttons : [
+          {
+            text : "好",
+            type : "button-positive"
+          }
+        ]
+      });
+    } else {
+      $scope.shortedLink = result;
+    }
+  }
+
+  $scope.shareLink = function (link) {
+    Share.doShare (link);
+  }
+}])
+
+.factory('intent', function($rootScope){
+    var intent = {data:null};
+
+    intent.setData = function(data){
+          intent.data = data;
+          console.log (data);
+          $rootScope.$broadcast('intent');
+        };
+
+    intent.getData = function(){
+          return intent.data;
+        };
+
+    return intent;
+});
---
+++ www/js/services.js
@@ -0,0 +1,32 @@
+angular.module('app.services', ['app.controllers'])
+
+.service ('NCUOne', function () {
+  this.lastLink = "";
+  this.lastShortLink = "";
+  this.shortLink = function (link) {
+    if (this.lastLink == link) {
+      return 1;
+    } else {
+      this.lastLink = link;
+      this.lastShortLink = "https://ncu.one/OAO";
+      return this.lastShortLink;
+    }
+  }
+})
+
+.service ('Share', function () {
+  this.doShare = function (link) {
+    var options = {
+        message: null,
+        subject: null,
+        files: null,
+        url: link,
+        chooserTitle: '選擇要分享的APP'
+    };
+    window.plugins.socialsharing.shareWithOptions (options, function (result) {
+      console.log (result);
+    }, function (msg) {
+      console.log(msg);
+    });
+  }
+})
