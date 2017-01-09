8e16a1ec3f2eb76c5bdc27d3b3a3b866720c1984
David Kuo <s50407s@gmail.com>
2017-01-09 11:50:20 +0800

    [Coding Style] Re-indent

--- www/js/services.js
+++ www/js/services.js
@@ -1,48 +1,48 @@
 angular.module('app.services', ['app.controllers'])
 
 .service ('NCUOne', function () {
-  this.lastLink = "";
-  this.lastShortLink = "";
-  this.shortLink = function (link) {
-    if (this.lastLink == link) {
-      return 1;
-    } else {
-      this.lastLink = link;
-      this.lastShortLink = "https://ncu.one/OAO";
-      return this.lastShortLink;
-    }
-  }
+	this.lastLink = "";
+	this.lastShortLink = "";
+	this.shortLink = function (link) {
+		if (this.lastLink == link) {
+			return 1;
+		} else {
+			this.lastLink = link;
+			this.lastShortLink = "https://ncu.one/OAO";
+			return this.lastShortLink;
+		}
+	}
 })
 
 .service ('Share', function () {
-  this.doShare = function (link) {
-    var options = {
-        message: null,
-        subject: null,
-        files: null,
-        url: link,
-        chooserTitle: '選擇要分享的APP'
-    };
-    window.plugins.socialsharing.shareWithOptions (options, function (result) {
-      console.log (result);
-    }, function (msg) {
-      console.log(msg);
-    });
-  }
+	this.doShare = function (link) {
+		var options = {
+			message: null,
+			subject: null,
+			files: null,
+			url: link,
+			chooserTitle: '選擇要分享的APP'
+		};
+		window.plugins.socialsharing.shareWithOptions (options, function (result) {
+			console.log (result);
+		}, function (msg) {
+			console.log(msg);
+		});
+	}
 })
 
 .factory('intent', ['$rootScope', function($rootScope){
-    var intent = {data:null};
+	var intent = {data:null};
 
-    intent.setData = function(data){
-          intent.data = data;
-          console.log (data);
-          $rootScope.$broadcast('intent');
-        };
+	intent.setData = function(data){
+		intent.data = data;
+		console.log (data);
+		$rootScope.$broadcast('intent');
+	};
 
-    intent.getData = function(){
-          return intent.data;
-        };
+	intent.getData = function(){
+		return intent.data;
+	};
 
-    return intent;
+	return intent;
 }]);
